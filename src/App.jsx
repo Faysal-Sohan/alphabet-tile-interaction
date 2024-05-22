634ab7a913097c781139afe211439f8dd36720a1
Faysal-Sohan <83866915+Faysal-Sohan@users.noreply.github.com>
2024-05-22 18:16:54 +0600

    added output string

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+const App = () => {
+
+  const [outputString, setOutputString] = useState("");
+  const [count, setCount] = useState(0);
+
+  const alphabets = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ").split('');
+
+  const colors = [];
+  for (let i = 0; i < 26; i++) {
+    const red = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+    const green = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+    const blue = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
+    colors.push(`bg-[#${red}${green}${blue}]`);
+  }
+
+  const handleString = (letter) => {
+    const len = outputString.length;
+    if (len == 0) {
+      setOutputString(outputString + letter)
+      setCount(1);
+      return;
+    }
+    if (outputString[len - 1] !== letter) {
+      setOutputString(outputString + letter);
+      setCount(1);
+    }
+    else {
+      let tempCount = count + 1;
+      setCount(count + 1);
+      if (tempCount < 3) {
+        setOutputString(outputString + letter);
+      }
+      else if (tempCount % 3 === 0) {
+        let newStr = "" + outputString;
+        while (newStr[newStr.length - 1] == letter) {
+          newStr = newStr.slice(0,-1);
+        }
+        setOutputString(newStr + "_");
+      }
+    }
+  }
+
+return (
+  <div className="container mx-auto">
+    <h3 className="text-3xl m-4">Output String: {outputString}</h3>
+    <div className="flex flex-wrap justify-center gap-4 h-32">
+      {
+        alphabets.map((letter, i) => <div key={i} className="bg-purple-200 text-center text-2xl p-4 rounded-md shadow-sm hover:bg-gray-300" onClick={() => handleString(letter)}>
+          {letter}
+        </div>)
+      }
+    </div>
+  </div>
+);
+};
+
+export default App;
